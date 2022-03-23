88a31eab31f8e3f7e7878101e6537fba3106e892
Emerson <emerson.borda.1994@gmail.com>
2022-03-23 15:28:27 -0300

    Trabajando en la navegación del sitio

--- libreria/urls.py
+++ libreria/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ #El  usuario accede a la vista. Redireccionamiento
-    path('', views.inicio, name='inicio'),
+    path('inicio', views.inicio, name='inicio'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear, name='crear'),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def inicio(request):
-    return HttpResponse("<h1>BienvenidoXD</h1>")
+def inicio(request): #función que permite acceder a la vista
+    return render(request, 'paginas/inicio.html')
+def nosotros(request):#Renderizar documento html
+    return render(request, 'paginas/nosotros.html')
+def libros(request):
+    return render(request, 'libros/index.html')
+def crear(request):
+    return render(request, 'libros/crear.html')
 
